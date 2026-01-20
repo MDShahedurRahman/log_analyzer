db11ec0cf5fdb25b5be8558fad38f8ea2714b1aa
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-19 23:28:00 -0500

    add to_dict method

--- log_entry.py
+++ log_entry.py
@@ -3,3 +3,10 @@ class LogEntry:
         self.date = date
         self.level = level
         self.message = message
+
+    def to_dict(self):
+        return {
+            "date": self.date,
+            "level": self.level,
+            "message": self.message
+        }
