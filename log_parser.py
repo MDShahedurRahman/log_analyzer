3a0ad7e968e14c7d38c98c2e833961653fe65aad
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-19 23:48:01 -0500

    add parse_log_line method

--- log_parser.py
+++ log_parser.py
@@ -1,1 +1,9 @@
 from log_entry import LogEntry
+
+
+def parse_log_line(line):
+    parts = line.strip().split(" ", 2)
+    if len(parts) != 3:
+        return None
+    date, level, message = parts
+    return LogEntry(date, level, message)
