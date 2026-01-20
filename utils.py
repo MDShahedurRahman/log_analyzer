a54bd02b6ff73b85faa86d0b2268f75ff016964a
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-19 23:52:18 -0500

    add date validation helper and log level normalization

--- utils.py
+++ utils.py
@@ -3,3 +3,9 @@ from utils import validate_date
 
 def filter_by_level(entries, level):
     return [e for e in entries if e.level == level]
+
+
+def filter_by_date(entries, date):
+    if not validate_date(date):
+        return []
+    return [e for e in entries if e.date == date]
