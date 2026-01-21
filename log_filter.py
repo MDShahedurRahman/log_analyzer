c39d0eea9cb604453e2bd37e755125b16a7e03c1
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-20 23:06:41 -0500

    add filter_by_date

--- log_filter.py
+++ log_filter.py
@@ -3,3 +3,7 @@ from utils import validate_date
 
 def filter_by_level(entries, level):
     return [e for e in entries if e.level == level]
+
+
+def filter_by_date(entries, date):
+    return [e for e in entries if e.date == date]
