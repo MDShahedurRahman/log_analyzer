02dfe60702ebd55baff0d84b7db1d3e94f0952b8
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-20 23:12:03 -0500

    create application entry point

---
+++ main.py
@@ -0,0 +1,12 @@
+from log_loader import load_logs
+from log_parser import parse_logs
+from log_filter import filter_by_level
+from analyzer import count_by_level
+from report import summary_report, print_report
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
